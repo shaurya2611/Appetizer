85aa327877d921a75934a9432f34ea6bb68106c9
Shaurya Singh <shaurya2611@gmail.com>
2023-10-29 17:48:34 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Shaurya Singh on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
